d4891430724836f2aa22f04fdbb23b8ba9936ccc
olatzaranzabe <olatzaranzabe@gmail.com>
2019-12-11 12:05:24 +0100

    find medium rate number

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -19,14 +19,25 @@ let howManyMovies = movies.filter(movie => {
     return movie.director === "Steven Spielberg";
 });
 
-console.log(howManyMovies);
+console.log(howManyMovies.length);
 
 const orderAlphabetically = movies => {
-    return movies.sort((a, b) => {
+    const sortMovies = movies.sort((a, b) => {
         if (a.title > b.title) return 1;
         if (a.title < b.title) return -1;
         return 0;
     });
+    return sortMovies.slice(0, 20);
 };
 
 console.log(orderAlphabetically(movies));
+
+// const twentyMovies = orderAlphabetically(movies).slice(0, 20);
+
+// console.log("20", twentyMovies);
+
+let rates = movies.map(({ rate }) => rate);
+
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+console.log((rates.reduce(reducer) / rates.length).toFixed(2));
