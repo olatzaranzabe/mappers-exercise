6c624c92de986c57c1b041f19c7a1d29b68b4267
olatzaranzabe <olatzaranzabe@gmail.com>
2019-12-11 10:50:42 +0100

    wip order alphabetically

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -20,3 +20,13 @@ let howManyMovies = movies.filter(movie => {
 });
 
 console.log(howManyMovies);
+
+const orderAlphabetically = movies => {
+    return movies.sort((a, b) => {
+        if (a.title > b.title) return 1;
+        if (a.title < b.title) return -1;
+        return 0;
+    });
+};
+
+console.log(orderAlphabetically(movies));
